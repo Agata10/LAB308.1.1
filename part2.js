56fca2fd4ab8b2a9acd1bbbfeff723e2b9bd3b69
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-14 15:46:46 -0600

    Check how long it take

--- part2.js
+++ part2.js
@@ -9,6 +9,9 @@ console.log(`Gallons if 55 miles per hour: ${sumOfGallons}`);
 //2.Will your budget be enough to cover the fuel expense?
 let isEnough = sumOfGallons * 3 <= budget;
 console.log(isEnough); //true
+//3.How long will the trip take, in hours?
+let howLong = distance / 55;
+console.log("How long, 55 miles per hour," + howLong);
 
 // 60 miles per hour - 28 miles per gallon
 //1.How many gallons of fuel will you need for the entire trip?
@@ -17,6 +20,9 @@ console.log(`Gallons if 60 miles per hour: ${sumOfGallons1}`);
 //2.Will your budget be enough to cover the fuel expense?
 let isEnough1 = sumOfGallons1 * 3 <= budget;
 console.log(isEnough1); //true
+//3.How long will the trip take, in hours?
+let howLong1 = distance / 60;
+console.log("How long, 55 miles per hour," + howLong1);
 
 //75 miles per hour - 23 miles per gallon
 //1.How many gallons of fuel will you need for the entire trip?
@@ -25,3 +31,6 @@ console.log(`Gallons if 75 miles per hour: ${sumOfGallons2}`);
 //2.Will your budget be enough to cover the fuel expense?
 let isEnough2 = sumOfGallons2 * 3 <= budget;
 console.log(isEnough2); //false
+//3.How long will the trip take, in hours?
+let howLong2 = distance / 75;
+console.log("How long, 75 miles per hour," + howLong2);
