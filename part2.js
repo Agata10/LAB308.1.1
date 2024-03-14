01d7c8791ff48e2dfeba06ac86b893c316c5402c
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-14 15:41:52 -0600

    How many gallons

---
+++ part2.js
@@ -0,0 +1,21 @@
+const distance = 1500;
+let budget = 175;
+let average = 3;
+
+// 55 miles per hour - 30 miles per gallon
+//1.How many gallons of fuel will you need for the entire trip?
+let sumOfGallons = 1500 * (1 / 30);
+console.log(`Gallons if 55 miles per hour: ${sumOfGallons}`);
+//2.Will your budget be enough to cover the fuel expense?
+
+// 60 miles per hour - 28 miles per gallon
+//1.How many gallons of fuel will you need for the entire trip?
+let sumOfGallons1 = 1500 * (1 / 28);
+console.log(`Gallons if 60 miles per hour: ${sumOfGallons1}`);
+//2.Will your budget be enough to cover the fuel expense?
+
+//75 miles per hour - 23 miles per gallon
+//1.How many gallons of fuel will you need for the entire trip?
+let sumOfGallons2 = 1500 * (1 / 23);
+console.log(`Gallons if 75 miles per hour: ${sumOfGallons2}`);
+//2.Will your budget be enough to cover the fuel expense?
